aa6bac2ad344ed679d46963d9e4a29fb8f58a80b
Peter <73311957+peter-korkinov@users.noreply.github.com>
2021-11-22 19:19:25 +0200

    fourth task

---
+++ 04.Fill-Dropdown/api.js
@@ -0,0 +1,47 @@
+const host = 'http://localhost:3030';
+
+async function request(url, options) {
+    try {
+        const response = await fetch(host + url, options);
+
+        if(response.ok !== true) {
+            if(response.status === 403) {
+                sessionStorage.removeItem('userData');
+            }
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        if (response.status === 204) {
+            return response;
+        } else {
+            return response.json();
+        }
+
+    } catch (err) {
+        alert(err.message);
+        throw err;
+    }
+}
+
+function createOptions(method='get', data) {
+    const options = {
+        method,
+        headers: {}
+    };
+
+    if(data !== undefined) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    return options;
+}
+
+export async function get() {
+    return request('/jsonstore/advanced/dropdown', createOptions());
+}
+
+export async function post(data) {
+    return request('/jsonstore/advanced/dropdown', createOptions('post', data));
+}
--- 04.Fill-Dropdown/dropdown.js
+++ 04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,27 @@
-function addItem() {
-    console.log('TODO:...');
+import {html, render} from '../node_modules/lit-html/lit-html.js';
+import {get, post} from './api.js';
+
+
+const dropMenu = document.getElementById('menu');
+const form = document.querySelector('form');
+form.addEventListener('submit', onSubmit);
+
+updateMenu();
+
+async function updateMenu() {
+    const records = await get();
+    const options = Object.keys(records).map(el => records[el]);
+
+    const elementTemplate = (options) => html`${options.map(option => html`<option value="${option._id}">${option.text}</option>`)}`;
+    render(elementTemplate(options), dropMenu);
+}
+
+async function onSubmit(event) {
+    event.preventDefault();
+
+    const text = new FormData(form).get('text');
+
+    form.reset();
+    await post({text});
+    updateMenu();
 }
